4a506aecb9cb76c6bfc0cb9a6d2f0517f8f5a88d
Joao Osorio <joao.l.osorio@gmail.com>
2024-01-19 00:21:39 +0000

    Managed session in profile through SSR.

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,26 +1,13 @@
-import { useSession } from 'next-auth/react';
-
 import ProfileForm from './profile-form';
 import styles from './user-profile.module.css';
 
 const UserProfile = () => {
-  const { data: session, status } = useSession();
-  const loading = status === 'loading';
-
-  if (loading) {
-    return <p className={styles.profile}>Loading...</p>;
-  }
-
-  if (session) {
-    return (
-      <section className={styles.profile}>
-        <h1>Your User Profile</h1>
-        <ProfileForm />
-      </section>
-    );
-  } else {
-    window.location.href = '/auth';
-  }
+  return (
+    <section className={styles.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm />
+    </section>
+  );
 };
 
 export default UserProfile;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,30 @@
+import { GetServerSidePropsContext } from 'next';
 import UserProfile from '../components/profile/user-profile';
+import { getSession } from 'next-auth/react';
 
 const ProfilePage = () => {
   return <UserProfile />;
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession({
+    req: context.req,
+  });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
 
 export default ProfilePage;
